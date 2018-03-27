cefb97c9b2bf0242af2db82e7e8ba05a692c2911
StepStudent <st@A212PC17.itstep.lan>
2018-03-27 19:37:57 +0300

    main in gzip

--- src/main/java/fileOperation/GZIPExtractor.java
+++ src/main/java/fileOperation/GZIPExtractor.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
 
@@ -16,11 +17,11 @@ public class GZIPExtractor {
         String newPath = newFolder + new File(fileName).getName();
         return unGZIP(newPath);
     }
-    
+
     public String unGZIP(String fileName) {
         byte[] buffer = new byte[bufferSize];
         String newName = fileName.substring(0, fileName.lastIndexOf("."));
-        
+
         try (GZIPInputStream gzipInput = new GZIPInputStream(new FileInputStream(fileName));
                 FileOutputStream output = new FileOutputStream(newName)) {
             int length = 0;
@@ -34,40 +35,43 @@ public class GZIPExtractor {
         }
         return newName;
     }
-    
+
     public String fromGzipToMemory(String fileName) {
-        StringBuilder content= new StringBuilder();
+        StringBuilder content = new StringBuilder();
         List<String> contentList = fromGzipToMemoryAsList(fileName);
-        
+
         for (String string : contentList) {
             content.append(string);
         }
         return content.toString();
     }
-    
+
     public List<String> fromGzipToMemoryAsList(String fileName) {
         String filePath = unGZIP(fileName);
-        
+
         ReaderFromFile reader = new ReaderFromFile(filePath);
         List<String> content = reader.readAllAsLIst();
         reader.close();
-        
+
         File file = new File(filePath);
         file.delete();
         return content;
     }
-    
-    public static void main (String[] args) {
-        List<String> list = new GZIPExtractor().fromGzipToMemoryAsList("d:\\Java\\Utils\\src\\main\\resources\\Games.csv.gz");
-        
+
+    public static void main(String[] args) {
+        URL url = GZIPExtractor.class.getClassLoader().getResource("Games.csv.gz");
+        List<String> list = new GZIPExtractor().fromGzipToMemoryAsList(url.getPath());
+
         int max = 0;
         String name = "";
         for (String string : list) {
             String[] split = string.split(";");
-            if (split[5].isEmpty()) {
-                System.out.println(split[0]);
+            if (split[1].length() > max) {
+                max = split[1].length();
+                name = split[1];
             }
+            if (split[2].isEmpty()) System.out.println(split[0]);
         }
-        System.out.println(max + ": " + name);
+        System.out.println(max + "\n" + name);
     }
 }
