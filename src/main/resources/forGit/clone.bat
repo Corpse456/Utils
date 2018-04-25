95efb4b154ce85c83a550ed003182c7e8758e212
Alexander Neznaev <Corpsetrup@gmail.com>
2018-04-25 11:06:18 +0400

    bat for git

---
+++ src/main/resources/forGit/clone.bat
@@ -0,0 +1,17 @@
+ECHO OFF
+git clone https://github.com/Corpse456/Utils.git
+git clone https://github.com/GitUser-J2817/AngryChess
+git clone https://github.com/Corpse456/Test.git
+git clone https://github.com/Corpse456/Crib.git
+mkdir BigProjects
+cd BigProjects
+git clone https://github.com/Corpse456/Calculator.git
+git clone https://github.com/Corpse456/ProceduralCHess.git
+git clone https://github.com/Corpse456/Notebook.git
+cd..
+mkdir Study
+cd Study
+git clone https://github.com/Corpse456/Hotels.git
+git clone https://github.com/Corpse456/Homeworks.git
+git clone https://github.com/Corpse456/Lessons.git
+git clone https://github.com/Corpse456/Tasks.git
---
+++ src/main/resources/forGit/pull.bat
@@ -0,0 +1,37 @@
+ECHO OFF
+cd Crib
+git pull
+cd..
+cd AngryChess
+git pull
+cd..
+cd Test
+git pull
+cd..
+cd Utils
+git pull
+cd..
+cd BigProjects
+cd Calculator
+git pull
+cd..
+cd Notebook
+git pull
+cd..
+cd ProceduralCHess
+git pull
+cd..
+cd..
+cd Study
+cd Homeworks
+git pull
+cd..
+cd Hotels
+git pull
+cd..
+cd Lessons
+git pull
+cd..
+cd Tasks
+git pull
+cd..
