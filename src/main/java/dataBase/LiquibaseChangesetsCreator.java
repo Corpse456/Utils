9ff934a0bd4ae354a3b0cf619c49252cffd632e3
Alexander Neznaev <Corpsetrup@gmail.com>
2018-08-19 21:06:10 +0200

    one more step

--- src/main/java/dataBase/LiquibaseChangesetsCreator.java
+++ src/main/java/dataBase/LiquibaseChangesetsCreator.java
@@ -6,9 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -18,53 +17,109 @@ import workWithFiles.fileIO.ReaderFromFile;
 
 public class LiquibaseChangesetsCreator {
 
-	public static void main(String[] args) {
-		new LiquibaseChangesetsCreator().findEnities("d:\\Me\\JAVA\\Study\\Kinopoisk\\");
-	}
+//    private static List<String> DATA_TYPES = Arrays.asList("String", "int", "Integer");
+    private static List<String> ACCESS_TYPES = Arrays.asList("protected", "public", "private");
+    private List<Entity> fields = new ArrayList<>();
 
-	private void findEnities(String destination) {
-		List<File> collect = walk(destination).filter(Files::isRegularFile)
-			.filter(f -> f.toFile().getName().endsWith(".java"))
-			.filter(f -> isEntity(f))
-			.map(Path::toFile)
-			.collect(Collectors.toList());
-		collect.forEach(e -> createTable(e));
-	}
+    public static void main (String[] args) {
+        new LiquibaseChangesetsCreator().findEnities("d:\\Me\\JAVA\\Study\\Kinopoisk\\");
+    }
 
-	private void createTable(File f) {
-		Map<String, Set<String>> fields = new HashMap<>();
-		ReaderFromFile reader = new ReaderFromFile(f);
-		
-		while(reader.isReady()) {
-			String currentLine = reader.readLine();
-			if (currentLine.startsWith("@")) {
-				fieldAdd(fields, reader, currentLine);
-			}
-		}
-	}
+    private void findEnities (String destination) {
+        List<File> collect = walk(destination)
+                .filter(Files::isRegularFile)
+                .filter(f -> f.toFile().getName().endsWith(".java"))
+                .filter(f -> isEntity(f)).map(Path::toFile)
+                .collect(Collectors.toList());
+        collect.forEach(e -> findEntities(e));
+        fields.forEach(a -> System.out.println(a));
+    }
 
-	private void fieldAdd(Map<String, Set<String>> fields, ReaderFromFile reader, String currentLine) {
-		Set<String> annotations = new TreeSet<>();
-		do {
-			annotations.add(currentLine.split("(")[0].replace("@", ""));
-			currentLine = reader.readLine();
-		} while (currentLine.contains("@"));
-		
-	}
+    private void findEntities (File f) {
+        ReaderFromFile reader = new ReaderFromFile(f);
+        while(!reader.readLine().contains("class " + f.getName().substring(0, f.getName().length() - 5)));
+        while (reader.isReady()) {
+            String currentLine = reader.readLine();
+            if (currentLine.contains("@")) {
+                fieldAdd(reader, currentLine);
+            }
+        }
+    }
 
-	private Stream<Path> walk(String destination) {
-		try {
-			return Files.walk(Paths.get(destination));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    private void fieldAdd (ReaderFromFile reader, String currentLine) {
+        Entity currentEntity = new Entity();
+        do {
+            String replace = currentLine.replaceAll(" ", "").replace("@", "");
+            currentEntity.addAnnotation(replace);
+            currentLine = reader.readLine();
+        } while (currentLine.contains("@"));
+        String[] fieldParametr = currentLine.split(" ");
+        for (int i = 0; i < fieldParametr.length; i++) {
+            if (ACCESS_TYPES.contains(fieldParametr[i])) {
+                currentEntity.setType(fieldParametr[i + 1]);
+                currentEntity.setName(fieldParametr[i + 2]);
+                fields.add(currentEntity);
+                return;
+            }
+        }
+    }
 
-	private boolean isEntity(Path f) {
-		ReaderFromFile reader = new ReaderFromFile(f.toFile());
-		String content = reader.readAll();
-		if (content.contains("@Entity")) return true;
-		else return false;
-	}
+    private Stream<Path> walk (String destination) {
+        try {
+            return Files.walk(Paths.get(destination));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean isEntity (Path f) {
+        ReaderFromFile reader = new ReaderFromFile(f.toFile());
+        String content = reader.readAll();
+        if (content.contains("@Entity")) return true;
+        else return false;
+    }
+
+    public class Entity {
+        private String name;
+        private Set<String> annotationList;
+        private String type;
+
+        public Entity () {
+            annotationList = new TreeSet<>();
+        }
+
+        public String getName () {
+            return name;
+        }
+
+        public void setName (String name) {
+            this.name = name;
+        }
+
+        public Set<String> getAnnotationList () {
+            return annotationList;
+        }
+
+        public void setAnnotationList (Set<String> annotationList) {
+            this.annotationList = annotationList;
+        }
+        
+        public void addAnnotation (String annotation) {
+            annotationList.add(annotation);
+        }
+        
+        public String getType () {
+            return type;
+        }
+
+        public void setType (String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String toString () {
+            return type + " " + name + " " + annotationList;
+        }
+    }
 }
