9f4dcd5d3c8926a87e8007be7299d5a9342c4919
Alexander Neznaev <Corpsetrup@gmail.com>
2019-02-28 23:33:18 +0300

    fix onliner

--- src/main/java/listCreators/onliner/Onliner.java
+++ src/main/java/listCreators/onliner/Onliner.java
@@ -1,14 +1,15 @@
 package listCreators.onliner;
 
-import java.io.IOException;
-
 import htmlConnector.HtmlExecutor;
 import workWithFiles.fileIO.WriterToFile;
 
+import java.io.IOException;
+
 public class Onliner {
 
-    private static final String PATH = "https://catalog.onliner.by/videocard";
-    private static final String COOKIE = "Gdyn=KlQL2RMGQMQG77DfsCnlNnE5ssGMjjLy7s4xLl8exgLif8QsG0Y6MHnMgM2GYlCFGiG8SlGSSGROguJiLRaGGSpASrYgsEgjhn4O0JGm3HW8IfrkQWW_2cjegmAv4AYeaFSG; ouid=snyBEFmupSGbyQvHw0KWAg==; ouid=snyBDlmupSN72gIIM6QmAg==; ouid=snyBDFmupTMvmU6Kv0r2Ag==; ouid=snyBDFnPXkR4My5WTK73Ag==; ouid=snyBDlmupSF7LwIHKa0ZAg==; tmr_detect=1%7C1519237940358";
+    private static final String PATH = "https://catalog.api.onliner.by/search/videocard";
+    private static final String COOKIE =
+        "Gdyn=KlQL2RMGQMQG77DfsCnlNnE5ssGMjjLy7s4xLl8exgLif8QsG0Y6MHnMgM2GYlCFGiG8SlGSSGROguJiLRaGGSpASrYgsEgjhn4O0JGm3HW8IfrkQWW_2cjegmAv4AYeaFSG; ouid=snyBEFmupSGbyQvHw0KWAg==; ouid=snyBDlmupSN72gIIM6QmAg==; ouid=snyBDFmupTMvmU6Kv0r2Ag==; ouid=snyBDFnPXkR4My5WTK73Ag==; ouid=snyBDlmupSF7LwIHKa0ZAg==; tmr_detect=1%7C1519237940358";
 
     public static void main(String[] args) throws IOException {
         HtmlExecutor exec = new HtmlExecutor();
