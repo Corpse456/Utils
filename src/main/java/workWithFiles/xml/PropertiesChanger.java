7af51040bf65c09921aeadf3f34614443d32200c
Alexander Neznaev <corpsetrup@gmail.com>
2019-08-19 11:44:37 +0300

    Changed propertiesChanger

--- src/main/java/workWithFiles/xml/PropertiesChanger.java
+++ src/main/java/workWithFiles/xml/PropertiesChanger.java
@@ -1,5 +1,13 @@
 package workWithFiles.xml;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,16 +19,53 @@ import java.util.Map;
 public class PropertiesChanger {
 
     public static void main(String[] args) throws Exception {
-        if (args.length < 2) {
-            System.out.println("Wrong usage");
-            System.exit(0);
+        CommandLine cmd = getCommandLine(args);
+
+        String[] inputFiles = cmd.getOptionValues("file");
+        String[] propertiesList = cmd.getOptionValues("property");
+
+        final Map<String, String> properties = getPropertiesMap(propertiesList);
+
+        for (final String inputFile : inputFiles) {
+            convert(inputFile, properties);
         }
+    }
+
+    private static Map<String, String> getPropertiesMap(final String[] args) {
         final Map<String, String> properties = new HashMap<>();
-        for (int i = 1; i < args.length; i++) {
-            final String[] split = args[i].split("=");
+        for (final String arg : args) {
+            final String[] split = arg.split("=");
             properties.put(split[0], split[1]);
         }
-        convert(args[0], properties);
+        return properties;
+    }
+
+    private static CommandLine getCommandLine(final String[] args) {
+        Options options = new Options();
+
+        addOption(options, "File path", "file");
+        addOption(options, "Properties", "property");
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("PropertiesChanger", options);
+
+            System.exit(1);
+        }
+        return cmd;
+    }
+
+    private static void addOption(final Options options, final String description, final String longOpt) {
+        Option file = new Option(longOpt.charAt(0) + "", longOpt, true, description);
+        file.setRequired(true);
+        file.setArgs(Option.UNLIMITED_VALUES);
+        options.addOption(file);
     }
 
     private static void convert(String xmlName, final Map<String, String> properties) throws Exception {
@@ -30,7 +75,7 @@ public class PropertiesChanger {
         while (bufferedReader.ready()) {
             String line = bufferedReader.readLine();
 
-            final String property = line.split("=")[0].replaceAll(" ", "");
+            final String property = line.split("=")[0].replaceAll(" ", "").replaceAll("#", "");
             if (properties.containsKey(property)) {
                 line = property + "=" + properties.get(property);
             }
@@ -38,8 +83,7 @@ public class PropertiesChanger {
         }
         bufferedReader.close();
 
-        final String newName = xmlName.replace(".properties", "_New.properties");
-        try (final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(newName)))) {
+        try (final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(xmlName)))) {
             bufferedWriter.write(result.toString());
         }
     }
